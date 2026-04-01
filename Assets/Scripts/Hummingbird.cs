60544647ba1b45c74a39ad1a90335502214da125
Madhur Rathod <madhur.thanku@gmail.com>
2026-04-01 22:28:33 +0530

    feat(Hummingbird): add variable declarations

--- Assets/Scripts/Hummingbird.cs
+++ Assets/Scripts/Hummingbird.cs
@@ -5,5 +5,22 @@ using Unity.MLAgents;
 /// </summary>
 public class Hummingbird : Agent
 {
+    [Tooltip("Force you apply when moving")]
+    public float moveForce = 2f;
+
+    [Tooltip("Speed to pitch up or down")]
+    public float pitchSpeed = 10f;
+
+    [Tooltip("Speed to rotate around the up axis")]
+    public float yawSpeed = 10f;
+
+    [Tooltip("Transform at the tip of the beak")]
+    public Transform beakTip;
+
+    [Tooltip("the agent's camera")]
+    public Camera agentCamera;
+
+    [Tooltip("whether this is training mode or gameplay mode")]
+    public bool trainingMode;
     
 }
