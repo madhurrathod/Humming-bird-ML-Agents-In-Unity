3e14417a2fcf4676b7c930ca39c6cd4cd0cc4c34
Madhur Rathod <madhur.thanku@gmail.com>
2026-04-07 00:24:54 +0530

    feat(Hummingbird): add private variables

--- Assets/Scripts/Hummingbird.cs
+++ Assets/Scripts/Hummingbird.cs
@@ -23,4 +23,34 @@ public class Hummingbird : Agent
     [Tooltip("whether this is training mode or gameplay mode")]
     public bool trainingMode;
     
+    // The rigidbody of the Agent
+    private new Rigidbody rigidbody;
+
+    // The flower area that the agent is in
+    private FlowerArea flowerArea;
+
+    // The nearest flower to the agent
+    private Flower nearestFlower;
+
+    // Allows for smoother pitch changes
+    private float smoothPitchChange = 0f;
+
+    // Allows for smoother yaw changes
+    private float smoothYawChange = 0f;
+
+    // maximum angle that the bird can pitch up or down
+    private const float MaxPitchAngle = 80f;
+
+    // maximum distance from the beakTip to accept nectar collision
+    private const float BeakTipRadius = 0.008f;
+
+    // whether the agent is frozen (intentionally not flying)
+    private bool frozen = false;
+
+    /// <summary>
+    /// The amount of nectar the agent has obtained this episode
+    /// </summary>
+    public float NectarObtained {get; private set;}
+
+    
 }
