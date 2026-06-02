79a61e52dd107048898a54607f2aca2daeece590
Madhur Rathod <madhur.thanku@gmail.com>
2026-06-02 23:27:30 +0530

    feat(HummingBird): TriggerEnterOrStay function implemented

--- Assets/Scripts/Hummingbird.cs
+++ Assets/Scripts/Hummingbird.cs
@@ -347,4 +347,59 @@ public class Hummingbird : Agent
 
         }
     }
+
+    /// <summary>
+    /// Called when the agent's collider enters a trigger collider 
+    /// </summary>
+    /// <param name="other">The trigger collider</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterOrStay(other);
+    }
+
+    /// <summary>
+    /// Called when the agent's collider stays a trigger collider 
+    /// </summary>
+    /// <param name="other">The trigger collider</param>
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerEnterOrStay(other);
+    }
+
+    private void TriggerEnterOrStay(Collider collider)
+    {
+        // check if agent is colliding with nectar
+        if (collider.CompareTag("nectar"))
+        {
+            Vector3 closestPointToBeakTip = collider.ClosestPoint(beakTip.position);
+
+            // check if the closest collision point is close to the beak tip
+            // Note: a collision with anything but the beak tip should not count
+            if(Vector3.Distance(beakTip.position, closestPointToBeakTip) < BeakTipRadius)
+            {
+                // Look up the flower for this nectar collider
+                Flower flower = flowerArea.GetFlowerFromNectar(collider);
+
+                // Attempt to take .01 nectar
+                // Note: this is per fixed timestamp, meaning it happens every .02 seconds, or 50x per second
+                float nectarReceived = flower.Feed(.01f);
+
+                // Keep track of nectar obtained
+                NectarObtained += nectarReceived;
+
+                if (trainingMode)
+                {
+                    // calculate reward for getting nectar
+                    float bonus = .02f * Mathf.Clamp01(Vector3.Dot(transform.forward.normalized, -nearestFlower.FlowerUpVector.normalized));
+                    AddReward(.01f + bonus);
+                }
+
+                // If flower is empty, update the nearest flower
+                if (!flower.HasNectar)
+                {
+                    UpdateNearestFlower();
+                }
+            }
+        }
+    }
 }
